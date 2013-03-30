2b96d9aaeb30296ea24ab430ca631aab5ef8f592
Bob Armstrong <rearmstr@gmail.com>
2013-03-30 01:00:21 -0400

    Added new configuration file to handle input parameters better

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 # GCC = g++
 # CXXFLAGS = -pg -g
 GCC = mpicxx
-CXXFLAGS = -O3
+CXXFLAGS = -O3 -g
 
 # ============ MBPbnl =======================================================
 # INCLUDE = -I/Users/mzm/opt/include -I/usr/local/include/CCfits
@@ -18,8 +18,8 @@ CXXFLAGS = -O3
 # LIBS = -L/astro/u/esheldon/exports/tmv-work/lib -ltmv -ltmv_symband -lblas -lpthread -lCCfits -lcfitsio
 
 # for folio
-INCLUDE = -I/usr/global/tmv/include -I/usr/global/CCfits/include -I/usr/global/cfitsio/include
-LIBS = -L/usr/global/tmv0.71/lib -ltmv -ltmv_symband -lmkl_intel_lp64 -lmkl_core   \
+INCLUDE = -I /data2/home/rarmst/soft/tmv0.71_icpc/include/ -I/usr/global/CCfits/include -I/usr/global/cfitsio/include
+LIBS = -L/data2/home/rarmst/soft/tmv0.71_icpc/lib -ltmv -ltmv_symband -lmkl_intel_lp64 -lmkl_core   \
        -lmkl_sequential -lpthread -Wl,-rpath=/usr/global/tmv0.71/lib \
        -openmp -L/usr/global/CCfits/lib -lCCfits -L/usr/global/CCfits/lib -lcfitsio
 
@@ -31,12 +31,18 @@ clean:
 	rm $(fileName)
 
 OBJtmv = $(fileName).o NR.o initialize.o myIO.o PCAcommon.o PCAuseSVD.o PCAuseEM.o \
-	PCAuseWiberg.o rmDefocus.o
+PCAuseWiberg.o rmDefocus.o ConfigFile.o
 
 $(fileName).o: $(fileName).cpp myClass.h
 	$(GCC) $(CXXFLAGS) -c -o $@ $(fileName).cpp $(INCLUDE)
 $(fileName): $(OBJtmv)
 	$(GCC) $(CXXFLAGS) -o $(fileName) $(OBJtmv) $(LIBS)
+
+#test_svd.o: test_svd.cc myClass.h
+#	$(GCC) $(CXXFLAGS) -c -o $@ test_svd.cc $(INCLUDE)
+#test_svd: test_svd.o NR.o initialize.o myIO.o PCAcommon.o PCAuseSVD.o PCAuseEM.o \
+#	PCAuseWiberg.o rmDefocus.o
+#	$(GCC) $(CXXFLAGS) -o test_svd $^ $(LIBS)
 
 NR.o: NR.cpp NR.h
 	$(GCC) $(CXXFLAGS) -c -o $@ NR.cpp
@@ -49,6 +55,9 @@ myIO.o: myIO.cpp myIO.h
 
 PCAcommon.o: PCAcommon.cpp PCAcommon.h
 	$(GCC) $(CXXFLAGS) -c -o $@ PCAcommon.cpp $(INCLUDE)
+
+ConfigFile.o: ConfigFile.cpp ConfigFile.h
+	$(GCC) $(CXXFLAGS) -c -o $@ ConfigFile.cpp $(INCLUDE)
 
 PCAuseSVD.o: PCAuseSVD.cpp PCAuseSVD.h
 	$(GCC) $(CXXFLAGS) -c -o $@ PCAuseSVD.cpp $(INCLUDE)
--- PSF_PCA.cpp
+++ PSF_PCA.cpp
@@ -33,7 +33,10 @@
 #include "PCAuseWiberg.h"
 #include "rmDefocus.h"
 
+#include "ConfigFile.h"
 using namespace std;
+std::ostream* dbgout = 0;
+bool XDEBUG = false;
 
 /* Matrix dimmension assumptions:
      Each column of Xmat is a data vector. It is for these vectors that
@@ -96,9 +99,81 @@ int main( int argc, char **argv )
 
   if ( myrank == ROOT ) {               // input parameters and data
 
-    readFileNames(inName,outName,"fileName.par");
-    readInputParams(contParam,SNAPmaskPct,tol,inName.controlParamFile.data());
 
+
+    ConfigFile params;
+    // These are all the defaults, but might as well be explicit.
+    params.setDelimiter("=");
+    params.setInclude("+");
+    params.setComment("#");
+    params.load(argv[1]);
+    for(int k=2;k<argc;k++) params.append(argv[k]);
+
+    
+    inName.inputDir         = params.read<string>("input_dir","./");
+    // Want to specify this in same file
+    inName.controlParamFile = inName.inputDir+params.read<string>("controlParmFile","inputParams.par");
+    // Why are these in a file?  They are all the same. Replace by general bounds
+    inName.chipBoundFile    = inName.inputDir+params.read<string>("chip_file","DESchipBound.dat");
+    inName.nameBaseSNAPpsf  = params.read<string>("snap_psf","");
+    inName.dirBaseDES       = params.read<string>("des_base","");
+    inName.runID_DES        = params.read<string>("des_run","");
+    inName.DESexpListFile   = params.read<string>("exp_list","");
+    inName.nameBaseZtab     = params.read<string>("defocus_table","");
+
+    outName.outputDir    = params.read<string>("output_dir","./");
+    outName.starRecFile  = params.read<string>("star_file","");
+    outName.gridXY2File  = params.read<string>("grid_file","");
+    outName.dataMatFile  = params.read<string>("data_file","");
+    outName.dataMaskFile = params.read<string>("mask_file","");     
+        				
+    std::string base_em= params.read<string>("em_name","em");  
+    outName.eigenVecEMfile = outName.outputDir+base_em+"_vec";
+    outName.eigenCoefEMfile  = outName.outputDir+base_em+"_coef";
+    outName.reconEMfile= outName.outputDir+base_em+"_recon";      
+    outName.reconErrEMfile= outName.outputDir+base_em+"_recon_err";
+    outName.reconErrPixEMfile= outName.outputDir+base_em+"_pix_err";
+    outName.reconErrExpEMfile= outName.outputDir+base_em+"_exp_err";
+
+    std::string base_svd= params.read<string>("svd_name","svd");  
+    outName.eigenVecSVDfile = outName.outputDir+base_svd+"_vec";
+    outName.eigenCoefSVDfile  = outName.outputDir+base_svd+"_coef";
+    outName.reconSVDfile= outName.outputDir+base_svd+"_recon";      
+    outName.reconErrSVDfile= outName.outputDir+base_svd+"_recon_err";
+    outName.reconErrPixSVDfile= outName.outputDir+base_svd+"_pix_err";
+    outName.reconErrExpSVDfile= outName.outputDir+base_svd+"_exp_err";
+    
+    outName.defocusIDfile     = params.read<string>("defocus_id_file","");      
+    outName.defocusCoeffFile  = params.read<string>("defocus_coef_file","");      
+      
+    contParam.iverbose = params.read<int>("verbose",1);
+    contParam.icSVD = params.read<int>("do_svd",0);
+    contParam.icEM = params.read<int>("do_em",0);
+    contParam.icWiberg = params.read<int>("do_wiberg",0);
+    contParam.icout = params.read<int>("cout",0);
+    contParam.icMissing = params.read<int>("missing",0);
+
+    contParam.icMean    = params.read<int>("mean_subtract",1);
+    contParam.icDefocus = params.read<int>("defocus",0);
+    contParam.kEigen    = params.read<int>("npca");
+    contParam.iterMax   = params.read<int>("max_iter");
+    contParam.iSeed     = params.read<int>("seed",1234567890);
+    contParam.icGetMat  = params.read<int>("get_mat",1);
+    contParam.readDESorSNAP = params.read<int>("des_or_snap",1);
+    contParam.nChips    = params.read<int>("nchips",1);
+    contParam.mc        = params.read<int>("x_cells");
+    contParam.nc        = params.read<int>("y_cells");
+    contParam.nShapelet = params.read<int>("n_shapelet",3);
+    // modified with nChips
+    contParam.nrows     = contParam.nChips*contParam.mc*contParam.nc*contParam.nShapelet;
+    
+    contParam.ncols     = params.read<int>("n_exp",0);
+    contParam.nzTabCol  = params.read<int>("nz_tab_col",0);
+    contParam.skip61    = params.read<int>("skip61",0);
+    
+    SNAPmaskPct         = params.read<double>("snap_mask",50);    
+    tol                 = params.read<double>("em_tol",1.e-6);
+    
     srand(contParam.iSeed);
     // srand(time(0));    // set initial seed value to system clock
     // testRandNum();
